9da7bd006d258b269e48aeb89ffc411a61c58eaf
Gagandeep Singh <singh.gagandeep3911@gmail.com>
2021-04-11 19:14:03 +0530

    Added JS for Needles movement

--- script.js
+++ script.js
@@ -40,3 +40,56 @@ toggle.addEventListener('click', () => {
     e.target.innerHTML = 'Light Mode';
   }
 });
+
+function setTime() {
+  const time = new Date();
+  const day = time.getDay();
+  const hours = time.getHours();
+  const month = time.getMonth();
+  const hoursForClock = hours % 12;
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+  const date = time.getDate();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  // Clock Needles
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    hoursForClock,
+    0,
+    11,
+    0,
+    360
+  )}deg)`;
+
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    minutes,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(
+    seconds,
+    0,
+    59,
+    0,
+    360
+  )}deg)`;
+
+  // Time in Digits
+  timeEl.innerHTML = `${hoursForClock}:${
+    minutes < 10 ? `0${minutes}` : minutes
+  } ${ampm}`;
+
+  // Date
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
+
+setInterval(() => {
+  setTime();
+}, 1000);
